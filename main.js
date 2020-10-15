a597d0d5b5a1a5b55242367d87cd6e38903af017
saabismi <vilho.syvahuoko@gmail.com>
2020-10-15 09:54:12 +0300

    small improvements to language changing

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 let langEn = document.getElementsByClassName("en");// get all elements in the "en" class
 let langFi = document.getElementsByClassName("fi"); // get all elements in the "fi" class
 
-console.log("skripti toimii");
-
-// Make the English elements visible and hide the Finnish elements
+// Set the language to English
 function langToEn() {
-    for (let i = 0; i < langEn.length; i++) {
-        langEn[i].style.display = "block";
-    }
+    // loop through all the Finnish elements and hide them
     for (let i = 0; i < langFi.length; i++) {
         langFi[i].style.display = "none";
     }
-    console.log("language is now set to English");
+    // loop through all the English elements and show them
+    for (let i = 0; i < langEn.length; i++) {
+        langEn[i].style.display = "block";
+    }
 }
 
-// Make the Finnish elements visible and hide the English elements
+// Set the language to Finnish
 function langToFi() {
+    // loop through all the English elements and hide them
     for (let i = 0; i < langEn.length; i++) {
         langEn[i].style.display = "none";
     }
+    // loop through all the Finnish elements and show them
     for (let i = 0; i < langFi.length; i++) {
         langFi[i].style.display = "block";
     }
-    console.log("language is now set to Finnish");
 }
